8e2c77329f1a2bd23741bfd0ee3dec5cfc20ece1
Chandler Abril <abrilchandler1@gmail.com>
2024-04-15 19:35:56 -0700

    Done

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,20 @@
 // Your tests here
+import { isPalindrome } from "../utils.js";
+
+describe("isPalindrome", () => {
+    it("determines if the string is the same reversed", () => {
+        const string = "racecar";
+
+        const palindrome = isPalindrome(string)
+
+        expect(palindrome).toBe(true)
+    });
+});
+
+it("handles uppercase and lowercase input", () => {
+    const string = "RaCeCaR";
+
+    const palindrome = isPalindrome(string);
+
+    expect(palindrome).toBe(true);
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,14 @@
 // Your code here
+export function isPalindrome(string) {
+    const arrayValues = string.split('');
+
+    const reverseArrayValues = arrayValues.reverse();
+
+    const reverseString = reverseArrayValues.join('');
+
+    if(string == reverseString) {
+        return true
+    } else {
+        return false 
+    }
+}
